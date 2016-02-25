a9745a9187f36829877ee965201c45bc14dd00a6
armando.dominguez <armando.dominguez@globant.com>
2016-02-25 11:37:35 -0600

    -commit from work

---
+++ app/src/main/java/com/globant/earthmonitor/viewmodel/DetailViewModel.java
@@ -0,0 +1,12 @@
+package com.globant.earthmonitor.viewmodel;
+
+import android.databinding.BaseObservable;
+
+/**
+ * Created by armando.dominguez on 25/02/2016.
+ */
+public class DetailViewModel extends BaseObservable {
+    private static final String TAG = DetailViewModel.class.getSimpleName();
+
+
+}
